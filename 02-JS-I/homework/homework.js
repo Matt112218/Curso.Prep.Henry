11f3a85eaf5c411998208f2b87f415f641575e3d
Matt112218 <you@example.com>
2021-08-10 11:34:39 -0300

    JS-I passed 30

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -154,8 +154,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-var Random = Math.random(0 , 1)
-return Random
+return Math.random()
 }
 
 function esPositivo(numero) {
@@ -163,24 +162,36 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+if (numero > 0) { 
+print('Es positivo')  
+}
+if (numero < 0) {
+print('Es negativo')  
+}
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var Excl = str + '!'
+  return Excl 
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  var combNombre = nombre + ' ' + apellido
+  return combNombre
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  var saludo = 'Hola ' + nombre +'!'
+  return saludo
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
