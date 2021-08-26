0a7e78de54d519fb5fb46c4af707c138df06c394
Matt112218 <you@example.com>
2021-08-26 12:05:06 -0300

    03-JS-II passed 33

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -107,7 +107,13 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-
+if (numero === 0){
+  return true
+}else if (numero % Math.floor(numero) === 0){
+  return true
+}else {
+  return false
+}
 }
 
 function fizzBuzz(numero) {
@@ -152,6 +158,11 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero === 0 || numero === 1 || numero === 4) return false;
+	for (let x = 2; x < numero / 2; x++) {
+		if (numero % x === 0) {return false
+    }else {return true}
+  }
 }
 
 function esVerdadero(valor){
@@ -170,16 +181,20 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  for (let i = 0; i < 11; i++) {
-    return array[i] * 6;
-    
-  }
+for (let i = 0; i = 10 ; i++) {
+  const element = array[i];
+  
+}
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero % Math.floor(numero) === 0 && numero.length === 3) {
+    return true    
+  } else {
+    return false
+  }
 }
 
 function doWhile(numero) {
