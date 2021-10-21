177e188c728f14f494d87a798a4fddbb40cc5e65
Matt112218 <you@example.com>
2021-10-21 20:48:39 -0300

    JSV passed 2

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,6 +8,19 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  class Usuario {
+    constructor(usuario, nombre, email, password) {
+      // var opciones = {}
+      this.usuario = usuario;
+      this.nombre = nombre;
+      this.email = email;
+      this.password = password;
+    }
+  }
+   saludar() ;{
+      return 'Hola, mi nombre es ' + this.nombre;
+  };
+   return Usuario 
 }
 
 function agregarMetodoPrototype(Constructor) {
@@ -35,10 +48,22 @@ function agregarStringInvertida() {
     //   Domicilio: 'Saavedra 123'
     //  }
 
-  class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+  class 
+  Persona {
+    constructor(nombre, apellido, edad, domicilio) {
       // Crea el constructor:
-
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio
+    }
+    detalle() {
+      var detalle = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        edad: this.edad,
+        domicilio: this.domicilio
+      }; return detalle
     }
 }
 
@@ -46,11 +71,16 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  var nuevaPersona = new Persona('Juan', 'Perez', 22, 'Saavedra123');
+  return nuevaPersona
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function() {
+return this.nombre +', ' + this.edad + ' años'
+  }
 }
   
 
